da95d8a2849b099e76de304a610745dea8b7b261
MartiHr <81310884+MartiHr@users.noreply.github.com>
2022-07-29 11:08:46 +0300

    Added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,9 @@
+import { Header } from "./components/header/Header";
 
 function App() {
     return (
         <div>
-            {/* Navigation */}
-            <header id="head">
-                <div className="nav nav-left">
-                    <a href="/">
-                        <img
-                            className="nav-logo nav-item"
-                            src="./static/favicons/astronaut.png"
-                            alt=""
-                        />
-                    </a>
-                    <h1>Spaceio</h1>
-                </div>
-                <div className="nav nav-right">
-                    <nav className="navigation">
-                        <ul     className="nav-wrapper">
-                            <li className="nav-item">
-                                <a href="/">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href="/ships">Ships</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href="/rockets">Rockets</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href="/about">About</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href="/help">Help</a>
-                            </li>
-                        </ul>
-                    </nav>
-                    <nav className="authentication">
-                        <ul className="nav-wrapper">
-                            <li className="nav-item">
-                                <a href="/login">Login</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href="/register">Register</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href="/logout">Logout</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
+            <Header />
             <main className="main">
                 <h1>Hello, Space Cadet!</h1>
                 <p>
